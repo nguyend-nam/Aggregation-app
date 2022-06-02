d3243eb07884992d62ef4d94e55876801cb6d3e8
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2022-06-02 15:33:31 +0700

    add sort criteria

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ let reposContainer = document.getElementById('repos_container');
 let sortDetails = document.getElementById('sort_details');
 let curInputVal = '';
 let repoList = [];
+let displayedRepoList = [];
 let repoIds = new Set();
 let curSortCriteria = 'stars';
 
@@ -14,28 +15,22 @@ function setSortCriteria(criteria) {
     
         sortDetails.innerHTML = '';
         sortDetails.append(sortInfo);
-        if(repoList.length != 0){
-            reposContainer.innerText = '';
-            showRepos();
-        }
+        
+        reposContainer.innerHTML = '';
+        showRepos();
     }
 }
 
 /* Process input data on Enter */
 input.addEventListener('keypress', function(event) {
     if(event.key == 'Enter'){
-        console.log(input.value);
-        if(input.value != '' && input.value != curInputVal){
-            repoList = [];
-            repoIds.clear();
-            curInputVal = input.value;
-            fetchAPI(input.value);
-        }
+        getInputVal();
     }
 });
 
 function getInputVal() {
     if(input.value != '' && input.value != curInputVal){
+        console.log(input.value);
         repoList = [];
         repoIds.clear();
         curInputVal = input.value;
@@ -51,42 +46,38 @@ async function fetchAPI(name) {
     for(let i=0; i<nameList.length; i++){
 
     	await fetch('https://api.github.com/users/' + nameList[i] + '/repos')
-    	.then(function(resp) {
+    	.then((resp) => {
             if (resp.ok) {
                 found = true;
                 return resp.json();
             }
-            throw new Error('Something went wrong');
-            
         }) // Convert data to json
-        .then(
-            data => {
-                if(data["message"] != "Not Found"){
-                    for(let k=0; k<data.length; k++){
-                        // apilist.push(data[k])
-                        if(repoIds.has(data[k]["id"])) continue;
-                        else{
-                            // apilist = apilist.concat(data);
-                            repoList.push(data[k])
-                            repoIds.add(data[k]["id"]);
-                        }
+        .then((data) => {
+            if(!("message" in data) && data["message"] != "Not Found"){
+                for(let k=0; k<data.length; k++){
+                    // apilist.push(data[k])
+                    if(repoIds.has(data[k]["id"])) continue;
+                    else{
+                        // apilist = apilist.concat(data);
+                        repoList.push(data[k])
+                        repoIds.add(data[k]["id"]);
                     }
                 }
             }
-    	)
+        })
         .catch((error) => { console.log(error) });
+        
         // If no users found start fetching API endpoint for organizations
         if(found == false){
             await fetch('https://api.github.com/orgs/' + nameList[i] + '/repos')
-            .then(function(resp) {
+            .then((resp) => {
                 if (resp.ok) {
                     return resp.json();
                 }
                 
             }) // Convert data to json
-            .then(
-                data => {
-                if(data["message"] != "Not Found"){
+            .then((data) => {
+                if(!("message" in data) && data["message"] != "Not Found"){
                     for(let k=0; k<data.length; k++){
                     // apilist.push(data[k])
                         if(repoIds.has(data[k]["id"])) continue;
@@ -105,10 +96,78 @@ async function fetchAPI(name) {
 }
 
 function showRepos() {
+    
+    displayedRepoList = repoList;
+    
+    /* Sort repoList */
+    if(curSortCriteria == 'stars') {
+        displayedRepoList.sort((b, a) => a.stargazers_count - b.stargazers_count);
+    }
+    else if(curSortCriteria == 'popular') {
+        displayedRepoList.sort(
+            (b, a) => (a.stargazers_count + a.forks_count + a.watchers_count) - (b.stargazers_count + b.forks_count + b.watchers_count)
+        );
+    }
+    else if(curSortCriteria == 'original') {
+        displayedRepoList = [];
+        for(let k=0; k<repoList.length; k++) {
+            if(repoList[k].fork == false){
+                displayedRepoList.push(repoList[k]);
+            }
+        }
+        displayedRepoList.sort(
+            (b, a) => (a.stargazers_count + a.forks_count + a.watchers_count) - (b.stargazers_count + b.forks_count + b.watchers_count)
+        );
+    }
+    
     let fragment = new DocumentFragment();
-    for(let i=0; i<repoList.length; i++){
-        fragment.append(repoList[i].full_name);
-        fragment.append(document.createElement('br'));
+    for(let i=0; i<displayedRepoList.length; i++){
+        
+        /* Create repo card */
+        let repoItem = document.createElement('div');
+        repoItem.classList.add('repo__card');
+        
+        /* Delete button */
+        let delButton = document.createElement('button');
+        delButton.innerText = 'X';
+        delButton.onclick = () => {
+            delButton.parentNode.style.maxHeight = '0';
+            delButton.parentNode.style.margin = '0';
+            delButton.parentNode.style.padding = '0 10px';
+            
+        }
+        
+        /* Title of repo card */
+        let repoTitle = document.createElement('a');
+        repoTitle.href = displayedRepoList[i].html_url;
+        repoTitle.target = '_blank';
+        repoTitle.innerText = displayedRepoList[i].full_name;
+        
+        /* Details of repo */
+        let starsCount = document.createElement('div');
+        starsCount.innerText = displayedRepoList[i].stargazers_count;
+        let language = document.createElement('div');
+        language.innerText = (displayedRepoList[i].language == null)?'--':displayedRepoList[i].language;
+        let forksCount = document.createElement('div');
+        forksCount.innerText = displayedRepoList[i].forks_count;
+        
+        let repoDetails = document.createElement('div');
+        repoDetails.classList.add('repo__details');
+        repoDetails.append(starsCount, language, forksCount);
+        
+        /* Avater of repo owner */
+        let repoAvatar = document.createElement('img');
+        repoAvatar.src = displayedRepoList[i].owner.avatar_url;
+        
+        let repoItemLeft = document.createElement('div');
+        repoItemLeft.append(repoTitle);
+        repoItemLeft.append(repoDetails);
+        
+        repoItem.append(repoItemLeft);
+        repoItem.append(repoAvatar);
+        repoItem.append(delButton);
+        fragment.append(repoItem);
+        
     }
     reposContainer.append(fragment);
 }
