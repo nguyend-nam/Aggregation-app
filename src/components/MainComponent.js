913dc5778e3c1bb6530be0bfd18c2c185b52faa0
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2022-08-12 16:16:32 +0700

    fix: add missing imports

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useRef } from 'react';
+import React, { Component, useState, useRef, useCallback, useEffect } from 'react';
 import OrgRepos from './ReposComponent';
 import Footer from './FooterComponent';
 
