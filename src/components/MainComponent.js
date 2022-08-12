5b5e52c7227b1acbc329b52533d250c134a307bc
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2022-08-12 16:14:48 +0700

    feat: focus on search input on slash key pressed

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,9 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useRef } from 'react';
 import OrgRepos from './ReposComponent';
 import Footer from './FooterComponent';
 
-function Main(){
-
+function Main() {
+  const searchInputRef = useRef(null)
   const [buffer, setBuffer] = useState('');
   const [inputval, setInputValue] = useState('');
   const [sortcriteria, setSortcriteria] = useState('stars');
@@ -14,7 +14,7 @@ function Main(){
     setBuffer(val);
   }
   const handleSubmit = () => {
-    if(buffer != ""){
+    if (buffer != "") {
       setIsLoading(true)
       setInputValue(buffer);
       console.log(buffer);
@@ -22,51 +22,65 @@ function Main(){
     }
   }
   const onKeyDown = (key) => {
-    if(key.charCode === 13) {
+    if (key.charCode === 13) {
       handleSubmit();
     }
   };
 
-  return(
-  <>
-    <div id="top"/>
-    <div className="header" id="header">
-      <div className="line"/>
-      <div className="title">
-        <a href="/"><img src="icons.png" height="55px" width="55px" alt="Aggregation App" />
-        <h1>Aggregation App</h1></a>
-        {/*<h1>{sortcriteria}</h1>*/}
-      </div>
+  const slashFunction = useCallback((event) => {
+    if (event.key === "/") {
+      searchInputRef.current.focus()
+    }
+  }, []);
 
-      <div className="searchbar">
-        <div>
-            <input required={true} className="input" id="name" type="text" placeholder="octocat, dwarvesf, etc."
-                onInput={updateInputValue}
-                onKeyPress={onKeyDown}/>
+  useEffect(() => {
+    document.addEventListener("keydown", slashFunction, false);
+
+    return () => {
+      document.removeEventListener("keydown", slashFunction, false);
+    };
+  }, []);
+
+  return (
+    <>
+      <div id="top" />
+      <div className="header" id="header">
+        <div className="line" />
+        <div className="title">
+          <a href="/"><img src="icons.png" height="55px" width="55px" alt="Aggregation App" />
+            <h1>Aggregation App</h1></a>
+          {/*<h1>{sortcriteria}</h1>*/}
+        </div>
+
+        <div className="searchbar">
+          <div>
+            <input ref={searchInputRef} required={true} className="input" id="name" type="text" placeholder="octocat, dwarvesf, etc."
+              onInput={updateInputValue}
+              onKeyPress={onKeyDown} />
             <button onClick={handleSubmit} className="ml-md-1"><i className="fas fa-search"></i></button>
+          </div>
+        </div>
+
+      </div>
+      <div className="sortbar">
+        <div>
+          <button onClick={() => setSortcriteria('stars')} className="ml-md-1">Stars</button>
+          <button onClick={() => setSortcriteria('popular')} className="ml-md-1">Popular</button>
+          <button onClick={() => setSortcriteria('original')} className="ml-md-1">Original</button>
+        </div>
+        <div className="sortinfo">
+          Current sort criterion: <span className="sortcrit">{sortcriteria.toUpperCase()}</span><br /><span>{(sortcriteria == "stars") ? "All repositories are ranked base on total stars count." : (sortcriteria == "popular") ? "All repositories are ranked base on sum of stars, watchers and forks count." : "Repositories (excluding forks) are ranked base on sum of stars, watchers and forks count."}</span>
         </div>
       </div>
-
-    </div>
-    <div className="sortbar">
-      <div>
-        <button onClick={() => setSortcriteria('stars')} className="ml-md-1">Stars</button>
-        <button onClick={() => setSortcriteria('popular')} className="ml-md-1">Popular</button>
-        <button onClick={() => setSortcriteria('original')} className="ml-md-1">Original</button>
+      <div className="content">
+        <OrgRepos name={inputval} sort={sortcriteria} loading={isLoading} setLoading={setIsLoading} />
+        <div className="intro">Input existing GitHub usernames and/or organization names into search bar to view repositories.<br />
+          If you want to search for <b>multiple</b> names, separate each of them by <b>comma</b>.</div>
       </div>
-      <div className="sortinfo">
-        Current sort criterion: <span className="sortcrit">{sortcriteria.toUpperCase()}</span><br/><span>{(sortcriteria == "stars")?"All repositories are ranked base on total stars count.":(sortcriteria == "popular")?"All repositories are ranked base on sum of stars, watchers and forks count.":"Repositories (excluding forks) are ranked base on sum of stars, watchers and forks count."}</span>
-      </div>
-    </div>
-    <div className="content">
-      <OrgRepos name={inputval} sort={sortcriteria} loading={isLoading} setLoading={setIsLoading} />
-      <div className="intro">Input existing GitHub usernames and/or organization names into search bar to view repositories.<br/>
-      If you want to search for <b>multiple</b> names, separate each of them by <b>comma</b>.</div>
-    </div>
 
-    <a className="scrollup" id="scrollup" href="#top"><i className="fas fa-long-arrow-alt-up"></i></a>
-    <Footer />
-  </>
+      <a className="scrollup" id="scrollup" href="#top"><i className="fas fa-long-arrow-alt-up"></i></a>
+      <Footer />
+    </>
   );
 }
 
